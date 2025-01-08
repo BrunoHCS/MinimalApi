4e9355792440a6f01616b317213e4e7c090e03f3
Bruno Henrique <bruno_hcs2009@hotmail.com>
2025-01-08 10:16:26 -0300

    ajustes

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/login", (MinimalApi.DTOs.LoginDTO loginDTO) =>
+app.MapPost("/login", (MinimalApi.Dominio.DTO.LoginDTO loginDTO) =>
 {
     if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
         return Results.Ok("Login com sucesso");
